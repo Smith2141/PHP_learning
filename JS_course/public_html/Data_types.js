ae700bf917b1afd7b0f509ca4474e741c6789e0b
smith_mint <berez_max@mail.ru>
2020-01-19 19:06:16 +0300

    JS first

---
+++ JS_course/public_html/Data_types.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var myName = "Ivan";
+var myAge = 39.5;
+var num = 39 > 40;
+var vacuum;
+
+
+//alert(typeof myAge);
+myAge;
