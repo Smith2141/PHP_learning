c5b4fcf7be8f38f1df782b7db50efae1ccb6b8dd
smith_mint <berez_max@mail.ru>
2020-01-22 22:38:46 +0300

    continuation cycles solution 2

--- JS_course/public_html/Additional_cycle_tasks.js
+++ JS_course/public_html/Additional_cycle_tasks.js
@@ -4,17 +4,34 @@
  * and open the template in the editor.
  */
 function testCycle(a, b) {
-    var x = "";
-    if (a > b) {
-        while (b < a) {
-            x += b++ + " ";
-        }
-        x += a;
-    } else {
-        while (a < b) {
-            x += a++ + " ";
-        }
-        x += b;
+    min = Math.min(a, b);
+    max = Math.max(a, b);
+    var x = max;
+    for (i = max - 1; i >= min; i--)
+        x += " " + i;
+    return x;
+}
+
+
+function testCycle(a, b) {
+    if (a < b)
+        [a, b] = [b, a];
+    let result = 0;
+    do {
+        result += a * a;
+        a--;
+    } while (a >= b);
+    return result;
+}
+
+
+   1---2---3---4---5
+5: 1 + 3 + 5 + 7 + 9
+
+function testCycle(n) {
+    var result = 0;
+    function calculating_an_element(num) {
+        return 2*n-1
     }
     return x;
 }
