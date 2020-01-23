9a30b111839e7233d22575c53efe6e2a6c187c09
smith_mint <berez_max@mail.ru>
2020-01-23 23:05:50 +0300

    continue recursion task

--- JS_course/public_html/Additional_cycle_tasks.js
+++ JS_course/public_html/Additional_cycle_tasks.js
@@ -1,37 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-function testCycle(a, b) {
-    min = Math.min(a, b);
-    max = Math.max(a, b);
-    var x = max;
-    for (i = max - 1; i >= min; i--)
-        x += " " + i;
-    return x;
-}
-
-
-function testCycle(a, b) {
-    if (a < b)
-        [a, b] = [b, a];
-    let result = 0;
-    do {
-        result += a * a;
-        a--;
-    } while (a >= b);
-    return result;
-}
-
-
-   1---2---3---4---5
-5: 1 + 3 + 5 + 7 + 9
+var result = "";
 
 function testCycle(n) {
-    var result = 0;
-    function calculating_an_element(num) {
-        return 2*n-1
+    for (i=1; i<=n; ++i) {
+        let x = 0;
+        function square(i) {
+            if (i == n) {
+                return x;
+            } else {
+                x += 2 * n - 1;
+                return square(++i);
+            }
+    }return += x + " ";
+    
     }
-    return x;
-}
