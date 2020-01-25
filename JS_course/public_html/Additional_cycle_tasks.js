2d1b65f7bf20f6299ccdc002d569e283779e2419
smith_mint <berez_max@mail.ru>
2020-01-25 21:24:01 +0300

    recursion task complete!

--- JS_course/public_html/Additional_cycle_tasks.js
+++ JS_course/public_html/Additional_cycle_tasks.js
@@ -1,15 +1,23 @@
-var result = "";
-
 function testCycle(n) {
-    for (i=1; i<=n; ++i) {
-        let x = 0;
-        function square(i) {
-            if (i == n) {
-                return x;
-            } else {
-                x += 2 * n - 1;
-                return square(++i);
-            }
-    }return += x + " ";
-    
+    var result = "";
+    var s = 0;
+    var itr = 0;
+    function square(i) {
+        if (i == 0) {
+            [s, itr] = [itr,s]
+            itr = 0;
+            return s;
+        } else {
+            itr += 2 * i - 1;
+            return square(--i);
+        }
     }
+    
+    for (let j=1; j<=n-1; ++j) {
+        result += square(j) + " ";
+    }
+    result+=square(n)
+    return result;
+}
+
+document.write(testCycle(8));
