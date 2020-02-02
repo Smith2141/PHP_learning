50ee284b9843cc7108c9d9fc44997ca84f0ea2ee
smith_mint <berez_max@mail.ru>
2020-02-02 22:03:35 +0300

    binary complete

--- JS_cource/2_15_To_the_binary_system.js
+++ JS_cource/2_15_To_the_binary_system.js
@@ -1,26 +1,13 @@
 function testCycle(n) {
-  var result = '*';
-  // var incomplete_private = parseInt(n / 2);
-  // var remainder_of_division = n % 2;
-  var i = 0;
-  // while (parseInt(n / 2) > 0) {
-  //   if (i == 20) {
-    //     break;
-  //   }
-  //   result = n + '>>' + n % 2 + '|' + result;
-  //   n = parseInt(n / 2);
-  do {
-    if (i == 20) {
+  var result = '';
+  while (parseInt(n / 2) >= 0) {
+    result = n % 2 + result;
+    n = parseInt(n / 2);
+    if (n == 0) {
       break;
     }
-    result = n + '>>' + n % 2 + '|' + result;
-    n = parseInt(n / 2);
-    ++i;
   }
-  while (parseInt(n / 2) >= 0 || n % 2 !== 0);
-  // }
   return result;
 }
-
 
 console.log(testCycle(75));
